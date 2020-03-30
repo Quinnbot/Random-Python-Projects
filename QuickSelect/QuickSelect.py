4a900cf23b47bd06ea4d19cb9510b52187fd6677
Quinn Montgomery <quinninthetardis@gmail.com>
2020-03-30 19:48:39 -0400

    quick select is mostly working. need to do some debuging

--- QuickSelect/QuickSelect.py
+++ QuickSelect/QuickSelect.py
@@ -1,14 +1,52 @@
 
+class sort():
 
-def QuickSelect(nums, k):
+    #nums = [61, 93, 56, 90, 11]
+    nums = [4, 1, 10, 8, 7, 12, 9, 2, 15]
 
-    lomuto(nums, k, k)
+    def changeArr(self, newArr):
+
+        self.nums = newArr
+
+    def swap(self, pos1, pos2):
+
+        temp = self.nums[pos1]
+        self.nums[pos1] = self.nums[pos2]
+        self.nums[pos2] = temp
+
+        return self.nums
+
+    def QuickSelect(self, l, r, k):
+
+        s = self.lomuto(0, 4)
+        if(s == k-1):
+
+            return self.nums[s]
+        
+        elif(s > l + r - 1):
+            self.QuickSelect(l, s - 1, k)
+
+        else:
+            self.QuickSelect(s + 1, r, k - 1 - s)
+
+    def lomuto(self, l, r):
+
+        P = self.nums[l]
+        s = l
+
+        for i in range(l + 1, r):
+
+            if(self.nums[i] < P):
+
+                s = s + 1
+                self.nums = self.swap(s, i)
+
+        self.nums = self.swap(l, s)
+        return s
 
 
+#testing
 
-def lomuto(nums, l, r):
+test = sort()
 
-    P = nums[l]
-    s = l
-
-
+print(test.QuickSelect(0, 10, 5))
